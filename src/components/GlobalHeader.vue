783fe63cdd69d3798991a75e6948f0a05b526435
shiqi.he <shiqi.he@xtalpi.com>
2024-12-22 12:51:24 +0800

    1222

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,85 @@
+<template>
+  <a-row :wrap="false">
+    <a-col flex="150px">
+      <div class="title-wrapper">
+        <img class="logo" src="@/assets/logo.png" alt="logo" />
+        <div class="title">测试</div>
+      </div>
+    </a-col>
+    <a-col flex="auto">
+      <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+    </a-col>
+    <a-col flex="80px">
+      <div class="user-login">
+        <a-button type="primary">登录</a-button>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined, UserOutlined, SettingOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+const current = ref<string[]>(['mail'])
+const items = ref<MenuProps['items']>([
+  {
+    key: 'first',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页'
+  },
+  {
+    key: 'secound',
+    icon: () => h(UserOutlined),
+    label: '用户登录',
+    title: '用户登录'
+  },
+  {
+    key: 'three',
+    icon: () => h(SettingOutlined),
+    label: '用户注册',
+    title: 'Navigation Three - Submenu',
+    children: [
+      {
+        type: 'group',
+        label: 'Item 1',
+        children: [
+          {
+            label: 'Option 1',
+            key: 'setting:1'
+          },
+          {
+            label: 'Option 2',
+            key: 'setting:2'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    key: 'four',
+    icon: () => h(SettingOutlined),
+    label: h('a', { href: 'https://antdv.com', target: '_blank' }, '用户管理'),
+    title: '用户管理'
+  }
+])
+</script>
+
+<style>
+.title-wrapper {
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+
+.title-wrapper .logo {
+  height: 30px;
+}
+
+.title-wrapper .title {
+  color: #070707;
+  margin-left: 20px;
+  font-size: 20px;
+}
+</style>
---
+++ src/layouts/BasicLayouts.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="basic-layouts">
+    <a-layout>
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.baidu.com" target="_blank">hsq的网站</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<style scoped>
+#basic-layouts .header {
+  background: #fff;
+  padding-inline: 10px;
+  margin-bottom: 10px;
+  color: unset;
+}
+#basic-layouts .content {
+  margin-bottom: 20px;
+  padding: 20px;
+  background: linear-gradient(to right, #d3b243, #fda085);
+}
+
+#basic-layouts .footer {
+  background-color: azure;
+  text-align: center;
+  position: fixed;
+  /* width: 100%; */
+  /* height: 50px; */
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  padding: 16px;
+}
+</style>
